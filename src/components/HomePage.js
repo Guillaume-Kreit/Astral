bc883c78313d9cbcfd06d307bc315f598ea305f8
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-29 21:54:20 +0200

    accept modif

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,11 +5,7 @@ import Name from './Name'
 const HomePage = () => {
     return(
         <div>
-<<<<<<< HEAD
             <h1>ASTRAL JOURNEY</h1>
-=======
-            <Name/>
->>>>>>> f7e09710ee3c168c6ded35fe5c3e912bd0e96e1b
             <Begin/>
         </div>
     )
