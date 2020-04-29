828b687084efe795be279962e6b4a2dc85b570f4
Boucqueau <gyboucqueau@hotmail.com>
2020-04-29 22:37:40 +0200

    Connexion aux webcams "mountain" "forest" "island"

--- src/components/Webcam.js
+++ src/components/Webcam.js
@@ -39,13 +39,72 @@ class Webcam extends React.Component {
           })
     }
 
-    componentDidMount = () => {
+    showMountainWebcam = () => {
+        this.setState({ category : 'mountain' })
         
+        axios
+          .get('https://api.windy.com/api/webcams/v2/list/category=mountain?key=20FwpHPZeZxGaR9V4acnJGDG0r5kTh04', {
+              method: 'get',
+            })
+          .then(res => {
+            console.log(res.data.result.webcams[0].id)
+                this.setState({
+                    isActive: true,
+                    id: res.data.result.webcams[0].id,
+                    });
+          })
+          .catch(err => {
+              console.log(err.message)
+          })
     }
 
+    showForestWebcam = () => {
+        this.setState({ category : 'forest' })
+        
+        axios
+          .get('https://api.windy.com/api/webcams/v2/list/category=forest?key=20FwpHPZeZxGaR9V4acnJGDG0r5kTh04', {
+              method: 'get',
+            })
+          .then(res => {
+            console.log(res.data.result.webcams[0].id)
+                this.setState({
+                    isActive: true,
+                    id: res.data.result.webcams[0].id,
+                    });
+          })
+          .catch(err => {
+              console.log(err.message)
+          })
+    }
+
+    showIslandWebcam = () => {
+        this.setState({ category : 'island' })
+        
+        axios
+          .get('https://api.windy.com/api/webcams/v2/list/category=island?key=20FwpHPZeZxGaR9V4acnJGDG0r5kTh04', {
+              method: 'get',
+            })
+          .then(res => {
+            console.log(res.data.result.webcams[0].id)
+                this.setState({
+                    isActive: true,
+                    id: res.data.result.webcams[0].id,
+                    });
+          })
+          .catch(err => {
+              console.log(err.message)
+          })
+    }
+    
     render () {
         return (
-        <div onClick={this.showBeachWebcam}>
+        <div>
+            <div>
+                <button onClick={this.showBeachWebcam}>BEACH</button>
+                <button onClick={this.showMountainWebcam}>MOUNTAIN</button>
+                <button onClick={this.showForestWebcam}>FOREST</button>
+                <button onClick={this.showIslandWebcam}>ISLAND</button>
+            </div>
             <iframe src={"https://webcams.windy.com/webcams/public/embed/player/"+this.state.id+"/lifetime"} style={this.styles.container}>
             </iframe>
         </div>
