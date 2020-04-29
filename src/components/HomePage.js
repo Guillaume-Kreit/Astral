c7070bbe03dc447cfcf1d03b0dbc6eef966a0349
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-29 21:03:40 +0200

    add component begin

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Begin from './Begin'
 
 const HomePage = () => {
     return(
         <div>
             <h1>Hello World</h1>
+            <Begin/>
         </div>
     )
 };
