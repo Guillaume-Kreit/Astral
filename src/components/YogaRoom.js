12ac133185a744cbb17b4bda1622efdbe07818be
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-04-30 08:45:25 +0200

    Push wallaper

--- src/components/YogaRoom.js
+++ src/components/YogaRoom.js
@@ -4,16 +4,15 @@ import Title from "./Title";
 import Timer from "./Timer";
 import Start from "./Start";
 // import Webcam from "./Webcam";
-//import zen from "../img/zen";
+import Wallaper from "./Wallaper";
 
 
 class YogaRoom extends React.Component {  
     styles = {
         container: {
-            backgroundColor : 'pink',
-            backgroundImage : "src('../img/zen.jpg') ",
-            width : '100%',
-            height : '100vh',
+            
+            
+            
         },
 
         left: {
@@ -24,13 +23,16 @@ class YogaRoom extends React.Component {
 
     render () {
         return (
-        <div style={this.styles.container}>
-            <Title/>
-            <Thematics style={this.styles.left} />
-            {/* <Webcam/> */}
-            <Timer />
-            <Start />
-        </div>
+            
+            <div style={this.styles.container}>
+                <Wallaper/>
+                <Title/>
+                <Thematics style={this.styles.left} />
+                {/* <Webcam/> */}
+                <Timer />
+                
+                <Start />
+            </div>
         )
     }
 }
