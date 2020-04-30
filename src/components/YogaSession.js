a65fc370aeb39a4995bab3b78f438d02b9c6679d
Boucqueau <gyboucqueau@hotmail.com>
2020-04-30 13:25:54 +0200

    YogaSession Breath

--- src/components/YogaSession.js
+++ src/components/YogaSession.js
@@ -1,21 +1,29 @@
 import React from "react";
 import Webcam from './Webcam';
 import Song from "./Song";
+import './YogaSession.css';
 
 
 class YogaSession extends React.Component {  
     styles = {
-        container: {
+        containerBox: {
             display: 'flex',
             height: '100vh',
             width: '100%',
             margin: '0px',
+            alignItems: 'center',
+            justifyContent: 'center',
         },
     }
 
     render () {
         return (
-        <div style={this.styles.container} >
+        <div style={this.styles.containerBox} >
+            <div style={{position:'absolute', top:'30%'}}> 
+                <div className="container">
+                    <div className="circle" />
+                </div>
+            </div>
             <Webcam {...this.props}/>
             <Song/>
         </div>
